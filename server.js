0e7ce8fca1655c1931a11bb0078f8d4a7fa2c252
joo-seongchan <jskjsj601@gmail.com>
2022-09-28 09:19:25 +0900

    methood

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var express = require("express");
-const { response } = require("express");
+// const { response } = require("express");
 
 var app = express();
 // app.use(function (request, response, next) {
@@ -99,19 +99,80 @@ var items = [
 // });
 
 app.use(express.static("public"));
-app.use(express.bodyPaser());
-app.use(express.Router);
+const bodyParser = require("body-parser");
+const { response } = require("express");
 
-app.get("/products", function (req, res) {
+app.use(bodyParser.json()); //요청 본문을 json 형태로 파싱
+app.use(bodyParser.urlencoded({ extended: false })); //
+
+app.get(`/products`, function (req, res) {
   res.send(items);
 });
 app.get(`/products/:id`, function (req, res) {
   var id = Number(req.param(`id`));
-  res.send(items[id]);
+
+  if (isNaN(id)) {
+    res.send({
+      error: `숫자를 입력하세요!`,
+    });
+  } else if (items[id]) {
+    res.send(items[id]);
+  } else {
+    res.send({ error: `존재하지않는 데이터입니다.` });
+  }
 });
-// app.post(`/products`, function (req, res) {});
-// app.put(`/products/:id`, function (req, res) {});
-// app.del(`/products/:id`, function (req, res) {});
+app.post(`/products`, function (req, res) {
+  var name = req.param("name");
+  var price = req.param(`price`);
+  var item = {
+    name: name,
+    price: price,
+  };
+  items.push(item);
+  res.send({
+    message: `데이터를 추가했습니다.`,
+    data: item,
+  });
+});
+app.put(`/products/:id`, function (req, res) {
+  var id = Number(req.param(`id`));
+  var name = req.param(`name`);
+  var price = req.param(`price`);
+
+  if (items[id]) {
+    if (name) {
+      items[id].name = name;
+    }
+    if (price) {
+      items[id].price = price;
+    }
+
+    res.send({
+      message: `데이터를 수정했습니다.`,
+      data: items[id],
+    });
+  } else {
+    res.send({
+      error: `존재하지 않는 데이터입니다!`,
+    });
+  }
+});
+app.del(`/products/:id`, function (req, res) {
+  var id = Number(req.param(`id`));
+
+  if (isNaN(id)) {
+    res.send({
+      error: `숫자를 입력하세요!`,
+    });
+  } else if (items[id]) {
+    items.splice(id, 1);
+    res.send({
+      message: `데이터를 삭제했습니다.`,
+    });
+  } else {
+    res.send({ error: `존재하지 않는 데이터입니다!` });
+  }
+});
 
 http.createServer(app).listen(5000, function () {
   console.log("sever Running at 10.10.10.119:5000");
